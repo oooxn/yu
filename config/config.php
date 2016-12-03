44e413b41397e2689b069c1f2d9a7b5925b54ed0
niuwenyu <niuwenyu@sailvan.com>
2016-12-03 11:20:01 +0800

    # 入口文件和配置文件

---
+++ config/config.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+ * 系统配置文件
+ *
+ */
+$CONFIG = array(
+	'system' => array(
+		//数据库
+		'db' => array(
+			'db_host' => 'localhost',
+			'db_user' => 'root',
+			'db_password' => '',
+			'db_database' => 'app',
+			'db_table_prefix' => 'app_',
+			'db_charset' => 'utf8',
+			'db_conn' => '',
+		),
+		'route' => array(
+			'default_controller' => 'home', //默认控制器
+			'default_action' => 'index', //系统默认控制器
+			'url_type' => 1,
+		),
+		'cache' => array(
+			'cache_dir' => 'cache', //
+			'cache_prefix' => 'cache_',
+			'cache_time' => 1800,
+			'cache_mode' => 2,
+		),
+	),
+);
