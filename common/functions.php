339860177c8075a27dc78f9102a44b28298ea31e
oooxn <thisnwy@163.com>
2016-12-13 18:43:34 +0800

    # 优化类文件加载+model链接

--- common/functions.php
+++ common/functions.php
@@ -8,3 +8,12 @@
 function getTest() {
 	return 'niuwenyu';
 }
+
+/**
+ * 实例化模型
+ * @param string  $modelName
+ */
+function M($modelName) {
+	$model = $modelName.'Model';
+	return new $model;
+}
---
+++ view/home.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>首页</title>
+</head>
+<body>
+别闹，我是首页
+</body>
+</html>
