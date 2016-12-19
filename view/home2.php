defc95fe6020975541c41c14946648eff4f9d86a
oooxn <thisnwy@163.com>
2016-12-19 23:33:11 +0800

    #调整

--- view/home2.php
+++ view/home2.php
@@ -3,7 +3,7 @@
  
 <head>
     <meta charset="utf-8">
-    <title>Slick Login</title>
+    <title>首页</title>
     <meta name="description" content="slick Login">
     <meta name="author" content="Webdesigntuts+">
     <link rel="stylesheet" type="text/css" href="resource/css/style.css" />
@@ -11,7 +11,7 @@
  
 <body>
     <form id="slick-login" method="post" action="index.php">
-        <label for="account">username</label><input type="text" name="account" class="placeholder" placeholder="me@tutsplus.com">
+        <label for="account">account</label><input type="text" name="account" class="placeholder" placeholder="nwy520@sina.com">
         <label for="password">password</label><input type="password" name="password" class="placeholder" placeholder="password">
         <input type="submit" value="Log In">
     </form>
